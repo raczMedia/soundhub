1eae8de8a845c5176b1301e02b7ca6478fc14cf8
Joe Racz <raczonline@gmail.com>
2022-02-24 00:03:35 -0500

    Colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "sh-blue": "#009BA6",
+        "sh-blue-dark": "#164054",
+        "sh-blue-darker": "#062F42",
+        "sh-blue-darkest": "#04293A",
+        "sh-orange": "#F37923",
+        "sh-white": "#E4E4E4",
+      },
+    },
   },
   plugins: [],
 };
