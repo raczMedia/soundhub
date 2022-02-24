2558feeb7b64e386ea410a611d43096978c830e2
Joe Racz <raczonline@gmail.com>
2022-02-24 00:50:41 -0500

    Updating views directory and adding sidebar components

---
+++ src/views/profile/ProfileSidebar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Profile Sidebar</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProfileSidebar",
+});
+</script>
---
+++ src/views/projects/ProjectsSidebar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Projects Sidebar</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProjectsSidebar",
+});
+</script>
