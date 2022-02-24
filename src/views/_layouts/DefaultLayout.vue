9de61872b6b3fa8ae3e993e71bf9d4238636aafe
Joe Racz <raczonline@gmail.com>
2022-02-24 00:49:59 -0500

    Adding sections to Layout and App.vue

---
+++ src/views/_layouts/DefaultLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="flex" aria-label="content">
+    <section aria-label="sidebar" class="w-52 flex-shrink-0">
+      <slot name="sidebar" />
+    </section>
+    <section aria-label="content" class="flex-grow">
+      <nav aria-label="navigation">
+        <router-link to="/">Discover</router-link>
+        <router-link to="/projects">Projects</router-link>
+        <router-link to="/profile">Profile</router-link>
+      </nav>
+      <slot name="content" />
+    </section>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "DefaultLayout",
+});
+</script>
